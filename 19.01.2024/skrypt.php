a0172ca2615dcf987df08eafc5ec40a69dda11c7
Miłosz Michta <88401081+milmilo1@users.noreply.github.com>
2024-01-19 14:30:19 +0100

    Add files via upload

---
+++ 19.01.2024/skrypt.php
@@ -0,0 +1,9 @@
+<?php
+echo "Dziękujemy! Rejestracja do szkolt przebiegła pomyślnie.<br>";
+echo "Wprowadzone dane:<br>";
+echo "Nazwisko: " trim($_POST['nazw']). "<br>";
+echo "Imie:". trim($_POST['im']). "<br>";
+echo "Zawód: ".trim($_POST['zaw']). "<br>";
+echo "Adres e-mail: ". trim($_POST['adr']). "<br>";
+echo ":<br>";
+?>
